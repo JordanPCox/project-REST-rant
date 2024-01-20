ea1a2b3f5964cd8d4225d19413417056c8112415
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-20 14:50:06 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = requires('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(3000, () => {
+    console.log("I'm alive!")
+})
