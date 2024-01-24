7b131d91a0971867d5bc0c5cae389fd0306fcc1d
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-24 12:58:17 -0500

    Added new route

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,14 @@
 const router = require('express').Router()
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+    const placeId = req.params.id
+    res.send(`Displaying details for place with ID $[placeId}`)
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
