2244cc8bc70461b456d2890d43160dd182cea2a3
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-27 22:31:00 -0500

    Fixed edit page

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,6 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -16,6 +12,47 @@ router.get('/:id', (req, res) => {
     else {
     res.render('places/show', { place: places[id], id })
     }
+})
+
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id], id})
+    }
+})
+
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+      if (isNaN(id)) {
+          res.render('error404')
+      }
+      else if  (!places[id]) {
+          res.render('error404')
+      }
+      else {
+          if (!req.body.pic) {
+          req.body.pic = 'http://placekitten.com/400/400'
+          }
+          if (!req.body.city) {
+              req.body.city='Anytown'
+          }
+          if (!req.body.state) {
+              req.body.state='USA'
+          }
+          places[id] = req.body
+          res.redirect(`/places/${id}`)
+      }
+  })
+
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
 })
 
 router.get('/', (req, res) => {
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,15 +1,15 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form () {
-    return (
+function new_form() {
+    return(
         <Def>
             <main>
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
                     <div className="form-group">
                         <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" required />
+                        <input className="form-control" id="name" name="name" required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="pic">Place Picture</label>
