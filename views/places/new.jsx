7fd876d6886a0469604b3a16da48a9008a55491a
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-30 20:46:18 -0500

    Adjusted styling on new page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,31 +7,33 @@ function new_form() {
             <main>
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
-                    <div className="form-group">
-                        <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" required/>
+                    <div className="row">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                            <label htmlFor="name">Place Name</label>
+                            <input className="form-control" id="name" name="name" required/>
+                        </div>
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input className="form-control" type="url" id="pic" name="pic" />
+                        </div>
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                            <label htmlFor="city">City</label>
+                            <input className="form-control" id="city" name="city" />
+                        </div>
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                            <label htmlFor="state">State</label>
+                            <input className="form-control" id="state" name="state" />
+                        </div>
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                            <label htmlFor="cuisines">Cuisines</label>
+                            <input className="form-control" id="cuisines" name="cuisines" required />
+                        </div>
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                        <label htmlFor="founded">Year Founded</label>
+                        <input className="form-control" id="founded" name="founded" />
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="pic">Place Picture</label>
-                        <input className="form-control" type="url" id="pic" name="pic" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" required />
-                    </div>
-                    <div className="form-group">
-                    <label htmlFor="founded">Year Founded</label>
-                    <input className="form-control" id="founded" name="founded" />
-                    </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                    <input className="btn btn-primary m-2" type="submit" value="Add Place" />
                 </form>
             </main>
         </Def>
